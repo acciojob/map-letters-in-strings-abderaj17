badc73d2a71c9379595dd990c873bad12ad34d83
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-02-02 17:38:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ function mapLetters(word) {
 let obj = {};
 	for(let i = 0; i<=word.length; i++){
 		if(obj[word[i]]){
-			
+			obj[word[i].push(i)];
+		}else{
+			obj[word[i]] = [i];
 		}
 	}
+	return obj;
 }
